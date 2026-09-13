58ea6eeac8e842083f3af5dff227f4e193ad7688
Yuvan Sankar <yuvansankar0106-ux@github.com>
2026-09-13 23:23:54 +0530

    editable profile

--- student/views.py
+++ student/views.py
@@ -29,3 +29,27 @@ def logout_view(request):
 def cia_marks(request):
     marks = ExamMark.objects.filter(student=request.user)
     return render(request, 'students/examination.html', {'marks': marks})
+def profile(request):
+    if not request.session.get('logged_in'):
+        return redirect('login')
+    
+    # Session la iruntha data edukkum, illana default
+    student_data = request.session.get('student_data', {
+        'name': '',
+        'age': '',
+        'roll_no': '',
+        'email': '',
+        'dept': ''
+    })
+    
+    if request.method == 'POST':
+        student_data = {
+            'name': request.POST.get('name'),
+            'age': request.POST.get('age'),
+            'roll_no': request.POST.get('roll_no'),
+            'email': request.POST.get('email'),
+            'dept': request.POST.get('dept')
+        }
+        request.session['student_data'] = student_data
+    
+    return render(request, 'students/profile.html', {'student': student_data})

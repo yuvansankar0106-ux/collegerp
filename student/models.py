afb0ede38d0ab5fc026e88c5a9897602e1ebb3e6
Yuvan Sankar <yuvansankar0106-ux@github.com>
2026-09-13 23:14:36 +0530

    fix profile with name age

--- student/models.py
+++ student/models.py
@@ -5,3 +5,15 @@ class ExamMark(models.Model):
     subject = models.CharField(max_length=100)
     cia_marks = models.IntegerField()
     semester = models.IntegerField()
+def profile(request):
+    if not request.session.get('logged_in'):
+        return redirect('login')
+    # Dummy student data - nee maathalam
+    student_data = {
+        'name': 'Yuvan',
+        'age': 20,
+        'roll_no': 'CS101',
+        'email': 'yuvan@college.com',
+        'dept': 'CSE'
+    }
+    return render(request, 'students/profile.html', {'student': student_data})

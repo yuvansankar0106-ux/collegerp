8052b62a540239abaad82e6083193130510d91fd
Yuvan Sankar <yuvansankar0106-ux@github.com>
2026-09-13 20:57:18 +0530

    fixed user import

--- student/models.py
+++ student/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 class ExamMark(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=100)

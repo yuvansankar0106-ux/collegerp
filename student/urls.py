f5b77994a63b14199e7bf50372ea9465e4d51cae
Yuvan Sankar <yuvansankar0106-ux@github.com>
2026-09-14 12:25:10 +0530

    fix root redirect

--- student/urls.py
+++ student/urls.py
@@ -1,7 +1,11 @@
+from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
+    path('', lambda request: redirect('/login/')),
+    path('admin/', admin.site.urls),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('home/', views.student_home, name='student_home'),
